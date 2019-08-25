e86a5d765cabcab9a974d43c4683f29c62c899a8
zacanger <zac@zacanger.com>
2019-08-25 12:48:33 -0600

    feat: upgrade for current archive format

--- config.sample.js
+++ config.sample.js
@@ -3,7 +3,7 @@ module.exports = {
   consumer_secret: '##',
   access_token_key: '##',
   access_token_secret: '##',
-  path: './tweets.csv', // path to the archive .csv file
+  path: './tweet.js', // path to the archive tweet.js file
   maxDate: false, // delete older than this eg: '2011-12-31 00:00:00 +0000'
   saveRegexp: [], // save tweets matching this regexp: eg ['#newavatar', '@\w+']
   callsInterval: 500 // ms
